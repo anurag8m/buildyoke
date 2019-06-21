31b1f71c9becec6b410cdfb6b0fc1a5eb2239ded
Anurag <anurag.mishra@sanganan.in>
2019-06-21 15:27:45 +0530

    first commit

---
+++ precache-manifest.eccc50473fe6a66e856e8550262122d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e287f3ce17aa0ef1f45094857624fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72f10e49e8ad67847b95",
+    "url": "/static/css/2.9a6e3d1e.chunk.css"
+  },
+  {
+    "revision": "bafec2df514efd5a6c77",
+    "url": "/static/css/main.43a9e7a0.chunk.css"
+  },
+  {
+    "revision": "72f10e49e8ad67847b95",
+    "url": "/static/js/2.82d3837f.chunk.js"
+  },
+  {
+    "revision": "bafec2df514efd5a6c77",
+    "url": "/static/js/main.e366fd01.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/static/media/lg.rotating-balls-spinner.cffa9d9d.gif"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
